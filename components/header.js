875181efa5b5624ef0c3bb6eb4db0c6a079630fc
StevenManner <87057354+StevenManner@users.noreply.github.com>
2022-01-09 16:09:12 -1000

    Commit from TeeportHQ

--- components/header.js
+++ components/header.js
@@ -21,22 +21,14 @@ const Header = (props) => {
             <h1 className="heading textSM">{props.heading13}</h1>
           </a>
         </Link>
-        <Link href="/profile">
-          <a className="link1">
-            <h1 className="heading1 textSM">{props.heading12}</h1>
-          </a>
-        </Link>
+        <h1 className="heading1 textSM">{props.heading12}</h1>
         <h1 className="heading2 textSM">{props.heading123}</h1>
         <h1 className="heading3 textSM">{props.heading121}</h1>
-        <Link href="/book-a-room">
-          <a className="link2">
-            <h1 className="heading4 textSM">{props.heading124}</h1>
-          </a>
-        </Link>
+        <h1 className="heading4 textSM">{props.heading124}</h1>
         <div data-type="MobileMenu" className="mobile-menu">
           <div className="top">
             <Link href="/about">
-              <a className="link3">
+              <a className="link1">
                 <h1 className="heading5 textSM">{props.heading11}</h1>
               </a>
             </Link>
@@ -48,7 +40,7 @@ const Header = (props) => {
           </div>
           <div className="mid">
             <Link href="/profile">
-              <a className="link4">
+              <a className="link2">
                 <div className="container">
                   <svg viewBox="0 0 1024 1024" className="icon02">
                     <path d="M576 706.612v-52.78c70.498-39.728 128-138.772 128-237.832 0-159.058 0-288-192-288s-192 128.942-192 288c0 99.060 57.502 198.104 128 237.832v52.78c-217.102 17.748-384 124.42-384 253.388h896c0-128.968-166.898-235.64-384-253.388z"></path>
@@ -58,7 +50,7 @@ const Header = (props) => {
               </a>
             </Link>
             <Link href="/blog-post">
-              <a className="link5">
+              <a className="link3">
                 <div className="container1">
                   <svg viewBox="0 0 1024 1024" className="icon04">
                     <path d="M917.806 229.076c-22.212-30.292-53.174-65.7-87.178-99.704s-69.412-64.964-99.704-87.178c-51.574-37.82-76.592-42.194-90.924-42.194h-496c-44.112 0-80 35.888-80 80v864c0 44.112 35.888 80 80 80h736c44.112 0 80-35.888 80-80v-624c0-14.332-4.372-39.35-42.194-90.924zM785.374 174.626c30.7 30.7 54.8 58.398 72.58 81.374h-153.954v-153.946c22.984 17.78 50.678 41.878 81.374 72.572zM896 944c0 8.672-7.328 16-16 16h-736c-8.672 0-16-7.328-16-16v-864c0-8.672 7.328-16 16-16 0 0 495.956-0.002 496 0v224c0 17.672 14.326 32 32 32h224v624z"></path>
@@ -117,13 +109,9 @@ const Header = (props) => {
             font-weight: 700;
             text-decoration: none;
           }
-          .link1 {
-            display: contents;
-          }
           .heading1 {
             color: #000000;
             font-weight: 700;
-            text-decoration: none;
           }
           .heading2 {
             color: #000000;
@@ -133,13 +121,9 @@ const Header = (props) => {
             color: #000000;
             font-weight: 700;
           }
-          .link2 {
-            display: contents;
-          }
           .heading4 {
             color: #000000;
             font-weight: 700;
-            text-decoration: none;
           }
           .mobile-menu {
             top: 0px;
@@ -163,7 +147,7 @@ const Header = (props) => {
             flex-direction: row;
             justify-content: space-between;
           }
-          .link3 {
+          .link1 {
             display: contents;
           }
           .heading5 {
@@ -188,7 +172,7 @@ const Header = (props) => {
             flex-direction: column;
             justify-content: space-between;
           }
-          .link4 {
+          .link2 {
             display: contents;
           }
           .container {
@@ -206,7 +190,7 @@ const Header = (props) => {
             color: var(--dl-color-secondary-400);
             margin-left: var(--dl-space-space-halfunit);
           }
-          .link5 {
+          .link3 {
             display: contents;
           }
           .container1 {
